7cf88e1f0333c278bdc2f27a57b42b5137d4101f
nnackpt <koonnack55@gmail.com>
2025-06-19 17:17:17 +0700

    AppsFunctions Types

---
+++ src/types/AppsFunctions.ts
@@ -0,0 +1,12 @@
+export interface AppsFunctions {
+    funcCode: string
+    appCode: string
+    name: string
+    desc?: string
+    funcUrl?: string
+    active?: boolean
+    createdBy?: string
+    createdDatetime?: string
+    updatedBy?: string
+    updatedDatetime?: string
+}
