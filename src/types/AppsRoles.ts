dd7c48da85a3265fb22684b88db9e2fdf567d57f
nnackpt <koonnack55@gmail.com>
2025-07-04 14:57:19 +0700

    Application Roles Types

---
+++ src/types/AppsRoles.ts
@@ -0,0 +1,12 @@
+export interface AppsRoles {
+    rolE_CODE: string
+    apP_CODE: string
+    name: string
+    desc?: string
+    homE_URL?: string
+    active?: boolean
+    createD_BY?: string
+    createD_DATETIME?: string
+    updateD_BY?: string
+    updateD_DATETIME?: string
+}
