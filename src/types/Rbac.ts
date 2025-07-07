31d4429432287ea9a200f95e7a5ed2b651be50fe
nnackpt <koonnack55@gmail.com>
2025-07-07 16:16:22 +0700

    RBAC API Types

---
+++ src/types/Rbac.ts
@@ -0,0 +1,10 @@
+export interface Rbac {
+    rbaC_CODE: string;
+    apP_CODE: string;
+    rolE_CODE: string;
+    funC_CODE: string;
+    createD_BY?: string;
+    createD_DATETIME?: string;
+    updateD_BY?: string;
+    updateD_DATETIME?: string;
+}
